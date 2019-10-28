dc5154a292e73cb2e5461402850822528538e848
Vinay <vinaysinghbb@gmail.com>
2019-10-29 00:08:48 +0530

    implemented pie chart to portfolio

--- src/containers/MyPortfolio.js
+++ src/containers/MyPortfolio.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 import Layout from './../components/Layout';
 
+//pie chart import
+import {Pie} from 'react-chartjs-2';
+
+const data = {
+	labels: [
+		'Total Supply',
+		'Total Sale',
+		'Other Supply'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+            '#E38627',
+            '#C13C37',
+            '#6A2135'
+		]
+	}]
+};
+
 class MyPortfolio extends Component {
 
     constructor(props){
@@ -15,7 +34,22 @@ class MyPortfolio extends Component {
         
         return (
             <Layout>
-                my portfolio
+                <div style={{width: '250px', height: '250px', margin: '0 auto'}}>
+                    <Pie data={data}
+                        width={200}
+                        height={200}
+                        options={{ maintainAspectRatio: true,
+                                    legend: {
+                                        display: false,
+                                        position: 'top',
+                                        labels: {
+                                            fontFamily: 'Barlow',
+                                            fontSize: 14
+                                        }
+                                    } 
+                            }}   
+                    />
+                </div>
             </Layout>
         )
     }
